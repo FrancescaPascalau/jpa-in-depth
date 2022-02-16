eb1362e2c7216500cf677f04ea51e456a079f350
FrancescaPascalau <pascalaufrancescaa@gmail.com>
2022-02-16 10:26:20 +0100

    Added Transactional to delete method.

--- data/src/main/java/com/francesca/pascalau/repository/CustomerRepository.java
+++ data/src/main/java/com/francesca/pascalau/repository/CustomerRepository.java
@@ -2,10 +2,16 @@ package com.francesca.pascalau.repository;
 
 import com.francesca.pascalau.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     Optional<Customer> findById(Long id);
+
+    @Modifying
+    @Transactional
+    void deleteById(Long id);
 }
